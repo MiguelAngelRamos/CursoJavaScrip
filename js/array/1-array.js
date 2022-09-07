b88ddf69aab1caf032af031474c2a02136ad047b
Miguel Ramos <return.infox@gmail.com>
2022-09-06 22:38:41 -0300

    codigo actualizado objetos

--- js/array/1-array.js
+++ js/array/1-array.js
@@ -2,21 +2,17 @@
 //* Nos sirve para agrupar elementos
 const numeros = [10, 55, 108]; // * una dimensión
 const amigos = new Array('Pedro', 'Juan', 'Diego');
-
 //* Un Arreglo puede contener cualquier tipo
 const arregloFull = ["Mouse", 50, false, "not", null, { nombre: 'Miguel'}];
 // console.log(arregloFull);
 // console.table(arregloFull);
 //* clt console.table
 //* clg console.log
-
 //* Matrices (vector bidimensional 2 dimensiones)
-
 const matrix = [
                 [1, 5, 6],
                 [14,55, 78]
                ];
-
 console.table(matrix);
 console.log(matrix[0][1]);
 console.log(matrix[1][2]);
---
+++ js/objetos/2-objetos.js
@@ -0,0 +1,25 @@
+//* Objetos sobre objetos
+
+const sitioWeb = {
+  nombre: 'Google',
+  link: { enlace: 'www.google.cl' },
+  redesSociales: {
+    youtube: {
+      enlace: 'youtube.com/google',
+      nombre: 'Google'
+    }
+  }
+};
+
+//* Como obtengo el enlace de youtube
+console.log(sitioWeb.redesSociales.youtube.enlace);
+
+//* Destructuring (Desmenuzar)
+// const { enlace, nombre } = sitioWeb.redesSociales.youtube;
+// console.log(enlace);
+
+const {nombre, link, redesSociales} = sitioWeb;
+const { youtube } = redesSociales;
+const {enlace} = youtube;
+
+console.log(enlace);
