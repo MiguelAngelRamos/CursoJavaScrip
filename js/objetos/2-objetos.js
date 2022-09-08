b24b184434ebd53f9cbf3e93c5c7d29b68882450
Miguel Ramos <return.infox@gmail.com>
2022-09-08 19:56:20 -0300

    alias de objetos y eliminacion

--- js/objetos/2-objetos.js
+++ js/objetos/2-objetos.js
@@ -15,11 +15,12 @@ const sitioWeb = {
 console.log(sitioWeb.redesSociales.youtube.enlace);
 
 //* Destructuring (Desmenuzar)
-// const { enlace, nombre } = sitioWeb.redesSociales.youtube;
-// console.log(enlace);
+//* const { enlace, nombre } = sitioWeb.redesSociales.youtube;
+//* console.log(enlace);
 
-const {nombre, link, redesSociales} = sitioWeb;
-const { youtube } = redesSociales;
-const {enlace} = youtube;
-
+// const {nombre, link, redesSociales} = sitioWeb;
+// const { youtube } = redesSociales;
+// const {enlace} = youtube;
+//* redesSociales: { youtube: { enlace }} (todo esto recibe el nombre de enlace)
+const {nombre, redesSociales, redesSociales: { youtube: { enlace }} } = sitioWeb;
 console.log(enlace);
